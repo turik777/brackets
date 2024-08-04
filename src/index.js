a656314c0f1c3c26ed2cdd4a23c939278f499bb2
turik777 <52492191+turik777@users.noreply.github.com>
2024-08-04 19:37:15 +0300

    feat: complete check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [];
+  let pair = {};
+  let stack = [];
+  
+  bracketsConfig.forEach(bracket => {
+    open.push(bracket[0]);
+    pair[bracket[1]] = bracket[0];
+  });
+  
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    
+    if (current === stack[stack.length - 1] && current === pair[current]) {
+      stack.pop();
+    } else if (open.includes(current)) {
+      stack.push(current);
+    } else if (pair[current] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  
+  return stack.length === 0;
 }
